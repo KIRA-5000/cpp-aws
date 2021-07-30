f316e19c09e95ed3b1f8165d971653c5caec0180
Mayank Srivastava <mayank.srivastava@AD.MSYSTECHNOLOGIES.COM>
2021-07-30 15:27:12 +0530

    List users\

--- include/user.hpp
+++ include/user.hpp
@@ -3,15 +3,19 @@
 
 #include <util.hpp>
 
-#include <aws/iam/IAMClient.h>
 #include <aws/iam/model/CreateUserRequest.h>
 #include <aws/iam/model/CreateUserResult.h>
-
+#include <aws/iam/model/CreateAccessKeyRequest.h>
+#include <aws/iam/model/CreateAccessKeyResult.h>
+#include <aws/iam/model/DeleteUserRequest.h>
+#include <aws/iam/model/ListUsersRequest.h>
+#include <aws/iam/model/ListUsersResult.h>
 
 auto CreateUser(const std::string& accessKey, const std::string& secretKey, const std::string& userName)
 {
   bool flag = false;
   Aws::SDKOptions options;
+  Aws::IAM::Model::AccessKey accessKeys;
 
   Aws::InitAPI(options);
   {
@@ -31,10 +35,101 @@ auto CreateUser(const std::string& accessKey, const std::string& secretKey, cons
     else
     {
       std::cout << "Successfully created IAM user " << userName << std::endl;
-      flag = true;
+
+      Aws::IAM::Model::CreateAccessKeyRequest request;
+      request.SetUserName(Aws::String(userName.c_str(), userName.size()));
+
+      auto outcome = iam.CreateAccessKey(request);
+
+      if (outcome.IsSuccess())
+      {
+        accessKeys = outcome.GetResult().GetAccessKey();
+
+        std::cout << "Access Key ID: " << accessKeys.GetAccessKeyId() << "\n";
+        std::cout << "Secret Access Key: " << accessKeys.GetSecretAccessKey() << "\n";
+
+        flag = true;
+      }
+      else
+      {
+        std::cout << "Error creating access key for IAM user " << userName << ":" << outcome.GetError().GetMessage() << std::endl;
+      }
     }
   }
   
+  Aws::ShutdownAPI(options);
+
+  return ((flag) ? 0 : -1);
+}
+
+auto DeleteUser(const std::string& accessKey, const std::string& secretKey, const std::string& userName)
+{
+  bool flag = false;
+  Aws::SDKOptions options;
+
+  Aws::InitAPI(options);
+  {
+    auto iam = InitializeIAMClient(
+      Aws::String(accessKey.c_str(), accessKey.size()), 
+      Aws::String(secretKey.c_str(), secretKey.size())
+    );
+
+    Aws::IAM::Model::DeleteUserRequest request;
+    request.SetUserName(Aws::String(userName.c_str(), userName.size()));
+
+    auto outcome = iam.DeleteUser(request);
+
+    if (!outcome.IsSuccess())
+    {
+      std::cout << "Error deleting IAM user " << userName << ": " << outcome.GetError().GetMessage() << std::endl;
+      goto end;
+    }
+
+    std::cout << "Successfully deleted IAM user " << userName << std::endl;
+    flag = true;
+  }
+
+end:
+
+  Aws::ShutdownAPI(options);
+
+  return ((flag) ? 0 : -1);
+}
+
+auto ListUsers(const std::string& accessKey, const std::string& secretKey)
+{
+  bool flag = false;
+  Aws::SDKOptions options;
+
+  Aws::InitAPI(options);
+  {
+    auto iam = InitializeIAMClient(
+      Aws::String(accessKey.c_str(), accessKey.size()), 
+      Aws::String(secretKey.c_str(), secretKey.size())
+    );
+
+    Aws::IAM::Model::ListUsersRequest request;
+
+    auto outcome = iam.ListUsers(request);
+
+    if (!outcome.IsSuccess())
+    {
+      std::cout << "Failed to list iam users:" << outcome.GetError().GetMessage() << std::endl;
+      goto end;
+    }
+
+    const auto &users = outcome.GetResult().GetUsers();
+
+    for (const auto &user : users)
+    {
+      std::cout << "UserName: " << user.GetUserName() << std::endl;
+      std::cout << "UserId: " << user.GetUserId() << std::endl;
+      std::cout << "Arn: " << user.GetArn() << std::endl << std::endl;
+    }
+  }
+
+end:
+
   Aws::ShutdownAPI(options);
 
   return ((flag) ? 0 : -1);
